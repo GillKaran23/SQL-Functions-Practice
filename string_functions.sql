c04449432fa212ce6985d02f00031a8f6c041c2b
GillKaran23 <gillkaran2352@gmail.com>
2024-08-25 12:56:22 +0530

    SQL Functions

--- string_functions.sql
+++ string_functions.sql
@@ -50,3 +50,43 @@ FROM
     employee_data
 WHERE 
 	last_name LIKE 'J%';
+
+
+-- 7. Trim Department Names: Trim any leading or trailing spaces from the department names.
+SELECT
+	id,
+    department,
+    TRIM(department) AS 'Trimmed'
+FROM
+    employee_data;
+
+
+-- 8. Find Substring in First Names: Find and list employees whose first_name contains the substring "an".
+SELECT
+	id,
+    first_name
+FROM
+    employee_data
+WHERE
+	INSTR(first_name, 'an') > 0;
+
+
+-- 9. Pad First Names: Pad the first_name to a length of 10 characters, adding asterisks (*) at the end.
+SELECT
+	id,
+    first_name,
+    LENGTH(first_name) AS 'Lenght Before Pad',
+    RPAD(first_name,10,'*') AS Padding,
+    LENGTH(RPAD(first_name,10,'*')) AS 'Length After Pad'
+FROM
+    employee_data;
+
+
+-- 10. Lowercase Last Names: Convert the last_name of each employee to lowercase.
+SELECT
+	id,
+    first_name,
+    last_name,
+    LOWER(last_name) AS 'Lower Names'
+FROM
+    employee_data;
